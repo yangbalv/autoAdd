406d5057526e1ee6d6d96d5528804f37a3ec446f
张天一 <1539626240@qq.com>
2023-07-05 01:09:43 +0800

    //第一步获取往期数据

--- src/main/java/ssq/service/Analysis.java
+++ src/main/java/ssq/service/Analysis.java
@@ -22,12 +22,53 @@ public class Analysis {
     public static void main(String[] args) {
         Analysis analysis = new Analysis();
         try {
-            analysis.step1();
+            String path = "http://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/findDrawNotice?name=ssq";
+            String cookie = analysis.step11(path);
+            System.out.println(cookie);
+            String message = analysis.step12(path, cookie);
+            System.out.println(message);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    //第一步获取往期数据
+//    第一步（1）进行预请求并拿到cookie
+    public String step11(String path) throws IOException {
+        URL url = new URL(path);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setInstanceFollowRedirects(false);
+        httpURLConnection.connect();
+        Map<String, List<String>> headerFields = httpURLConnection.getHeaderFields();
+        for (Map.Entry<String, List<String>> stringListEntry : headerFields.entrySet()) {
+            System.out.println("key is: " + stringListEntry.getKey() + " value is: " + stringListEntry.getValue());
+        }
+
+        return httpURLConnection.getHeaderField("Set-Cookie");
+    }
+
+    //    第一步（2）将cookie塞到请求中进行数据获取
+    public String step12(String path, String cookie) throws IOException {
+
+        URL url = new URL(path);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setInstanceFollowRedirects(false);
+        httpURLConnection.setRequestProperty("Cookie", cookie);
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.connect();
+        Map<String, List<String>> headerFields = httpURLConnection.getHeaderFields();
+        for (Map.Entry<String, List<String>> stringListEntry : headerFields.entrySet()) {
+            System.out.println("key is: " + stringListEntry.getKey() + " value is: " + stringListEntry.getValue());
+        }
+        StringBuilder stringBuffer = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(), "UTF-8"));
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuffer.append(line + "\n");
+        }
+        return stringBuffer.toString();
+    }
 
     public void step1() throws IOException {
         String html = "http://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/findDrawNotice?name=ssq";
